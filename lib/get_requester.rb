1797f0161e49c6ece3e05cc0d87cdb1f285946a8
lordish <gitonga403@gmail.com>
2023-05-31 19:04:34 +0300

    final commit

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,30 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    def initialize
+        @base_url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    end
+
+    def get_response_body 
+        uri = URI.parse(@base_url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        # programs = JSON.parse(self.get_response_body)
+        # programs.collect do |program|
+        #     program["name"]
+        # end
+        response_body = get_response_body
+        JSON.parse(response_body)
+
+    end
+
+end
+
+programs = GetRequester.new
+puts programs.parse_json
